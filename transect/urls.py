1ab7392c6c9041838163f6d50ac72c72559cf409
thegreatleapforward <thegreatleapf@gmail.com>
2023-06-26 11:00:18 -0700

    added boilerplate

--- transect/urls.py
+++ transect/urls.py
@@ -1,9 +1,11 @@
 # transect/urls.py
 from django.urls import path
 
-from transect.views import index
+from transect.views import index, observation, transect
 
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
+    path("observation/<int:obvs_id>/", observation, name='observation'),
+    path("transect/<int:transect_id>/", transect, name='transect'),
 ]
--- transect/views.py
+++ transect/views.py
@@ -1,16 +1,36 @@
 # transect/views.py
+import json
 from datetime import datetime
-
+from django.shortcuts import render
+from django.views.generic import TemplateView
 from django.http import HttpResponse
+import aiohttp
+
 
 def index(request):
     now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    return render(request, 'transect/index.html', context={'now': now})
+
+
+async def observation(request, obvs_id):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f'https://thegreatleapforward.pythonanywhere.com/observations/{obvs_id}/?format=json') as response:
+            data = await response.json()
+
+    return render(request, 'transect/observation.html', context={**data})
+
+
+def upload(request):
+    return render(request, 'transect/upload.html')
+
+
+def api(request):
+    pass
+
+
+async def transect(request, transect_id):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f'https://thegreatleapforward.pythonanywhere.com/transects/{transect_id}') as response:
+            data = await response.json()
+
+    return render(request, 'transect/transect.html', context={'data': data})
